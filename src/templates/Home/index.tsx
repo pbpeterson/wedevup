70d088dedc5846410dc357f6ba0d022a19d8de01
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-02-01 14:23:36 -0300

    feat: add pagination

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import CardCar, { CardCarProps } from '../../components/CardCar'
 import SelectFilter from '../../components/Select'
@@ -7,7 +7,14 @@ import * as S from './styles'
 
 const HomeTemplate = () => {
   const [cars, setCars] = useState<CardCarProps[]>()
+  const itensPerPage = 6
+  const [currentPage, setCurrentPage] = useState<number>(0)
   const { priceByDay, priceByKm } = useFilter()
+  const startIndex = currentPage + itensPerPage
+  const endIndex = startIndex + itensPerPage
+  const currentCars = cars?.slice(startIndex, endIndex)
+
+  const totalPages = cars?.length ? Math.ceil(cars.length / itensPerPage) : 0
 
   useEffect(() => {
     const getCars = async () => {
@@ -20,6 +27,10 @@ const HomeTemplate = () => {
     }
     getCars()
   }, [priceByDay, priceByKm])
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setCurrentPage(Number(event.currentTarget.value))
+  }
   return (
     <>
       <S.Wrapper>
@@ -28,10 +39,23 @@ const HomeTemplate = () => {
           <SelectFilter selectType="km" />
         </S.SelectWrapper>
         <S.Cars>
-          {cars?.map((car) => (
+          {currentCars?.map((car) => (
             <CardCar key={car.model} {...car} />
           ))}
         </S.Cars>
+        <S.PaginatorWrapper style={{ color: 'white' }}>
+          {Array.from(Array(totalPages), (item, index) => {
+            return (
+              <S.PagintorItem
+                isActive={index == currentPage}
+                value={index}
+                onClick={handleClick}
+              >
+                {index + 1}
+              </S.PagintorItem>
+            )
+          })}
+        </S.PaginatorWrapper>
       </S.Wrapper>
     </>
   )
--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.div`
   max-width: 110rem;
@@ -24,4 +24,46 @@ export const Cars = styled.div`
   @media (max-width: 45rem) {
     grid-template-columns: 1fr;
   }
+`
+
+export const PaginatorWrapper = styled.div`
+  padding: 2rem;
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+`
+
+type PaginatorItemProps = {
+  isActive: boolean
+}
+
+const PagintorItemModifier = {
+  isActive: () => css`
+    background-color: #ba0108;
+  `,
+}
+
+export const PagintorItem = styled.button<PaginatorItemProps>`
+  ${({ isActive }) => css`
+    padding: 2rem;
+    width: 3rem;
+    height: 3rem;
+    font-size: 1.5rem;
+    font-weight: bold;
+    border-radius: 0.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: transparent;
+    cursor: pointer;
+    color: #fff;
+    border: 0.2rem solid #ba0108;
+    transition: background 0.4s;
+
+    &:hover {
+      background-color: #ba0108;
+    }
+
+    ${isActive && PagintorItemModifier.isActive()}
+  `}
 `

88adc646a322e8d1a639aa721382034078af46f8
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-02-01 03:29:04 -0300

    feat: add header to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
+import Header from '../components/Header'
 import { FilterProvider } from '../hooks/filter'
 import GlobalStyles from '../styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Header />
       <FilterProvider>
         <Component {...pageProps} />
         <GlobalStyles />
